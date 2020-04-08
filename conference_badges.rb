82631eb05bf4820e47b1c9d4698a4366fe33d7f2
Grace Berman <grace.g.berman@gmail.com>
2020-04-08 19:27:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,5 @@
-names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def badge_maker(names)
-  names.each do |name|
+def badge_maker(name)
     puts "Hello, my name is #{name}."
-  end
 end
 
 def batch_badge_creator(names)
